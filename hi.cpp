1acf3d85b5c1b7d5473bdadf9b9556501ca599ef
ahmed55777730 <158777392+ahmed55777730@users.noreply.github.com>
2024-03-10 22:22:15 +0000

    Problem # 17/2 Solution Using C++

--- hi.cpp
+++ hi.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
-#include <string>
-using namespace std;
 
-void PrintAllPattern()
+using namespace std;
+string ReadPassword(string msg){
+  cout << msg << endl;
+  string pass;
+  cin >> pass;
+  return pass;
+}
+bool PrintTrailsForPass(string pass)
 {
-  for (int i = 0; i <26; i++)
+  
+  int Trial = 0;        
+  string word = "";
+  for (int i = 0; i < 26; i++)
   {
     for (int j = 0; j < 26; j++)
     {
       for (int k = 0; k < 26; k++)
       {
-        cout << char(i + 65) << char(j + 65) << char(k + 65) << endl;
+        word += char(i + 65);
+        word += char(j + 65);
+        word += char(k + 65);
+        cout << "Trail "<<Trial++<<" for pass is : "<<word<< endl;
+        if (word == pass)
+        {
+          cout << "\nPassword is " << word << "\n";
+          cout << "Found after ";
+          cout << Trial << " Trial(s)\n";
+          return true;
+        }
+        word = "";
       }
-      
     }
-        
   }
+  return false;
 }
 
 int main()
 {
-  PrintAllPattern();
+  PrintTrailsForPass(ReadPassword("Write The Password that is a word from three letters capital : "));
   return 0;
 }
