3297a35837fdc5f3d66bc04f367632d813937c90
ahmed55777730 <158777392+ahmed55777730@users.noreply.github.com>
2024-03-10 15:14:32 +0000

    Problem # 8/2 Solution Using C++

--- hi.cpp
+++ hi.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int ReadPositiveNumber(string msg)
 {
   cout << msg;
@@ -8,23 +9,30 @@ int ReadPositiveNumber(string msg)
   cin >> num;
   return num;
 }
-int GetDigit(int Number)
+
+int NumberDigit(int Number)
 {
   return Number % 10;
 }
-void PrintNumberInReverse(int num)
-{    
-  int result = 0;
-  int i = 1;
-  while (num)
+
+void PrintNumberFrequency(int num1, int num2)
+{
+  int freq = 0;
+  while (num1)
   {
-    result = result*10 + GetDigit(num);
-    num = num / 10;
-  }    
-  cout << result << endl;
+    if (num2 == NumberDigit(num1))
+    {
+      freq++;
+    }
+    num1 /= 10;
+  }
+  cout << "Digit " << num2 << " Frequency is " << freq << " Time(s)" << endl;
 }
+
 int main()
 {
-  PrintNumberInReverse(ReadPositiveNumber("Please enter a positive number "));
+  int frequency = ReadPositiveNumber("Please enter a Frequency number: ");
+  int positiveNumber = ReadPositiveNumber("Please enter a positive number: ");
+  PrintNumberFrequency(frequency, positiveNumber);
   return 0;
 }
