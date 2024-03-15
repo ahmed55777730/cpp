d368ee4e04113fe9268145ccb4ab85775fe39a39
ahmed55777730 <158777392+ahmed55777730@users.noreply.github.com>
2024-03-15 14:36:48 +0000

    Problem # 18/2 Solution Using C++

--- hi.cpp
+++ hi.cpp
@@ -1,43 +1,32 @@
 #include <iostream>
 
 using namespace std;
-string ReadPassword(string msg){
-  cout << msg << endl;
-  string pass;
-  cin >> pass;
-  return pass;
-}
-bool PrintTrailsForPass(string pass)
+enum enEncreptionOrDecreption
 {
-  
-  int Trial = 0;        
-  string word = "";
-  for (int i = 0; i < 26; i++)
+  Encreption = 1,
+  Decreption = -1
+};
+string ReadWord(string msg)
+{
+  cout << msg << endl;
+  string word;
+  cin >> word;
+  return word;
+}
+void PrintEncreptionOrDecreption(string word, enEncreptionOrDecreption type, int key)
+{
+  string newWord = "";
+
+  for (int i = 0; i < word.size(); i++)
   {
-    for (int j = 0; j < 26; j++)
-    {
-      for (int k = 0; k < 26; k++)
-      {
-        word += char(i + 65);
-        word += char(j + 65);
-        word += char(k + 65);
-        cout << "Trail "<<Trial++<<" for pass is : "<<word<< endl;
-        if (word == pass)
-        {
-          cout << "\nPassword is " << word << "\n";
-          cout << "Found after ";
-          cout << Trial << " Trial(s)\n";
-          return true;
-        }
-        word = "";
-      }
-    }
+    newWord+=char(int(word[i])+type*key);
   }
-  return false;
+  cout << newWord <<endl;
 }
 
 int main()
 {
-  PrintTrailsForPass(ReadPassword("Write The Password that is a word from three letters capital : "));
+  int key = 2;
+  PrintEncreptionOrDecreption(ReadWord("Write The Password that is a word from three letters capital : "), enEncreptionOrDecreption::Decreption, key);
   return 0;
 }
