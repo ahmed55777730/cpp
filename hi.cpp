8e4ef586ee9fc2e7664b54298b90632ce4ecedc7
ahmed55777730 <158777392+ahmed55777730@users.noreply.github.com>
2024-03-15 14:43:00 +0000

    Problem # 19/2 Solution Using C++

--- hi.cpp
+++ hi.cpp
@@ -1,32 +1,12 @@
 #include <iostream>
-
 using namespace std;
-enum enEncreptionOrDecreption
-{
-  Encreption = 1,
-  Decreption = -1
-};
-string ReadWord(string msg)
-{
-  cout << msg << endl;
-  string word;
-  cin >> word;
-  return word;
-}
-void PrintEncreptionOrDecreption(string word, enEncreptionOrDecreption type, int key)
-{
-  string newWord = "";
-
-  for (int i = 0; i < word.size(); i++)
-  {
-    newWord+=char(int(word[i])+type*key);
-  }
-  cout << newWord <<endl;
-}
-
+  void PrintRandomNumbers(){
+      int N = 10;
+  for (int i = 0; i < 3; i++)
+    cout << rand() % N << endl;
+  };
 int main()
 {
-  int key = 2;
-  PrintEncreptionOrDecreption(ReadWord("Write The Password that is a word from three letters capital : "), enEncreptionOrDecreption::Decreption, key);
+  PrintRandomNumbers();
   return 0;
 }
