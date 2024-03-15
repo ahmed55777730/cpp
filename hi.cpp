1f06d9f7e5db9712a50d3f1cb2008905053a7f0c
ahmed55777730 <158777392+ahmed55777730@users.noreply.github.com>
2024-03-15 21:31:55 +0000

    Problem # 19/2 Solution Using C++

--- hi.cpp
+++ hi.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
-  void PrintRandomNumbers(){
-      int N = 10;
-  for (int i = 0; i < 3; i++)
-    cout << rand() % N << endl;
-  };
+enum enCharacterCategory
+{
+  lowercaseLettersStart = 97, 
+  lowercaseLettersEnd = 122,  
+
+  uppercaseLettersStart = 65,
+  uppercaseLettersEnd = 90,   
+
+  numbersStart = 48,
+  numbersEnd = 57, 
+
+  specialCharsStart = 33, 
+  specialCharsEnd = 47  
+};
+
+int randomNum(int From, int To)
+{
+
+  int randNum = rand() % (To - From + 1) + From;
+  return randNum;
+}
+char numToChar(int Num){
+  return char(Num);
+}
 int main()
 {
-  PrintRandomNumbers();
+  // Seeds the random number generator in C++, called only once
+  srand((unsigned)time(NULL));
+  cout << numToChar(randomNum(enCharacterCategory::lowercaseLettersStart,enCharacterCategory::lowercaseLettersEnd)) << endl;
+  cout << numToChar(randomNum(enCharacterCategory::uppercaseLettersStart,enCharacterCategory::uppercaseLettersEnd)) << endl;
+  cout << numToChar(randomNum(enCharacterCategory::numbersStart,enCharacterCategory::numbersEnd)) << endl;
+  cout << numToChar(randomNum(enCharacterCategory::specialCharsStart,enCharacterCategory::specialCharsEnd)) << endl;
+
   return 0;
 }
