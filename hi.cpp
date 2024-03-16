ed3f470e54e22480ef7226daec07cefee146c716
ahmed55777730 <158777392+ahmed55777730@users.noreply.github.com>
2024-03-16 02:32:32 +0000

    Problem # 29/2 Solution Using C++

--- hi.cpp
+++ hi.cpp
@@ -25,11 +25,37 @@ void ReadRandomArray(int arr[100], int &arrLength)
     arr[i] = RandomNumber(0, 100);
   }
 }
-void CopyArray (int arr[100],int arr2[100],int arrLength){
+bool isPrime(int number)
+{
+  // Handling special cases
+  if (number <= 1)
+    return false;
+  if (number <= 3)
+    return true;
+
+  // Check if number is divisible by 2 or 3
+  if (number % 2 == 0 || number % 3 == 0)
+    return false;
+
+  // Check for divisibility starting from 5 up to the square root of the number
+  for (int i = 5; i * i <= number; i += 6)
+  {
+    if (number % i == 0 || number % (i + 2) == 0)
+      return false;
+  }
+
+  // If no divisors are found, the number is prime
+  return true;
+}
+void CopyArray(int arr[100], int arr2[100], int arrLength,int &newLength)
+{
+  int index = 0;
   for (int i = 0; i < arrLength; i++)
   {
-    arr2[i] = arr[i];
+    if (isPrime(arr[i]))
+      arr2[index++] = arr[i];
   }
+  newLength = index;
 }
 void PrintArray(int arr[100], int arrLength)
 {
@@ -40,11 +66,14 @@ void PrintArray(int arr[100], int arrLength)
 int main()
 {
   srand((unsigned)time(NULL));
-  int arr[100],arr2[100], arrLength;
+  int arr[100], arr2[100], arrLength,newLength;
   ReadRandomArray(arr, arrLength);
-  CopyArray(arr, arr2, arrLength);
-          cout
+  CopyArray(arr, arr2, arrLength,newLength);
+  cout
       << "\nThe copy array: ";
-  PrintArray(arr2, arrLength);
+  PrintArray(arr, arrLength);
+    cout
+      << "\nThe copy array: ";
+  PrintArray(arr2, newLength);
   return 0;
 }
