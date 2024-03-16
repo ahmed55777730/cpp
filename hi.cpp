caf1a022a3e350c3c83d88678e6540ca79db310c
ahmed55777730 <158777392+ahmed55777730@users.noreply.github.com>
2024-03-16 03:10:25 +0000

    Problem # 31/2 Solution Using C++

--- hi.cpp
+++ hi.cpp
@@ -1,7 +1,7 @@
+
 #include <iostream>
 using namespace std;
 int ReadPositiveNumber(string Message)
-
 {
   int Number = 0;
   do
@@ -16,42 +16,43 @@ int RandomNumber(int From, int To)
   int randNum = rand() % (To - From + 1) + From;
   return randNum;
 }
-void ReadRandomArray(int arr[100], int arrLength)
-{
-  for (int i = 0; i < arrLength; i++)
-  {
-    arr[i] = RandomNumber(0, 100);
-  }
-}
+
 void PrintArray(int arr[100], int arrLength)
 {
   for (int i = 0; i < arrLength; i++)
     cout << arr[i] << " ";
   cout << "\n";
 }
-void PrintSumTwoArrays(int arr[100],int arr2[100], int arrLength)
+void OrderedArray(int arr[100], int arrLength)
 {
   for (int i = 0; i < arrLength; i++)
-    cout << arr[i] +arr2[i]<< " ";
-  cout << "\n";
+  {
+    arr[i] = i+1;
+  }
 }
-
+void ShuffleArray(int arr[100], int arrLength)
+{
+  for (int i = 0; i < arrLength; i++)
+  {
+    int Temp = arr[i];
+    int Random = RandomNumber(0, arrLength);
+    arr[i] = arr[Random];
+    arr[Random] = Temp;
+  }
+}
 int main()
 {
   srand((unsigned)time(NULL));
-  int arr[100], arr2[100];
+  int arr[100];
   int arrLength = ReadPositiveNumber("Write the length of array");
-  ReadRandomArray(arr, arrLength);
-  ReadRandomArray(arr2, arrLength);
+  OrderedArray(arr, arrLength);
   cout
-      << "\nThe first array: ";
+      << "\nThe ordered array: ";
   PrintArray(arr, arrLength);
+  ShuffleArray(arr, arrLength);
   cout
-      << "\nThe second array: ";
-  PrintArray(arr2, arrLength);
-  cout
-      << "\nThe sum of two arrays: ";
-  PrintSumTwoArrays(arr,arr2, arrLength);
+      << "\nThe shuffled array: ";
+  PrintArray(arr, arrLength);
 
   return 0;
 }
