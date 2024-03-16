4ee595eef49553c862541581e86b23c66968bae4
ahmed55777730 <158777392+ahmed55777730@users.noreply.github.com>
2024-03-16 02:44:02 +0000

    Problem # 30/2 Solution Using C++

--- hi.cpp
+++ hi.cpp
@@ -16,46 +16,12 @@ int RandomNumber(int From, int To)
   int randNum = rand() % (To - From + 1) + From;
   return randNum;
 }
-void ReadRandomArray(int arr[100], int &arrLength)
+void ReadRandomArray(int arr[100], int arrLength)
 {
-  cout << "\nEnter number of elements:\n";
-  cin >> arrLength;
   for (int i = 0; i < arrLength; i++)
   {
     arr[i] = RandomNumber(0, 100);
   }
-}
-bool isPrime(int number)
-{
-  // Handling special cases
-  if (number <= 1)
-    return false;
-  if (number <= 3)
-    return true;
-
-  // Check if number is divisible by 2 or 3
-  if (number % 2 == 0 || number % 3 == 0)
-    return false;
-
-  // Check for divisibility starting from 5 up to the square root of the number
-  for (int i = 5; i * i <= number; i += 6)
-  {
-    if (number % i == 0 || number % (i + 2) == 0)
-      return false;
-  }
-
-  // If no divisors are found, the number is prime
-  return true;
-}
-void CopyArray(int arr[100], int arr2[100], int arrLength,int &newLength)
-{
-  int index = 0;
-  for (int i = 0; i < arrLength; i++)
-  {
-    if (isPrime(arr[i]))
-      arr2[index++] = arr[i];
-  }
-  newLength = index;
 }
 void PrintArray(int arr[100], int arrLength)
 {
@@ -63,17 +29,29 @@ void PrintArray(int arr[100], int arrLength)
     cout << arr[i] << " ";
   cout << "\n";
 }
+void PrintSumTwoArrays(int arr[100],int arr2[100], int arrLength)
+{
+  for (int i = 0; i < arrLength; i++)
+    cout << arr[i] +arr2[i]<< " ";
+  cout << "\n";
+}
+
 int main()
 {
   srand((unsigned)time(NULL));
-  int arr[100], arr2[100], arrLength,newLength;
+  int arr[100], arr2[100];
+  int arrLength = ReadPositiveNumber("Write the length of array");
   ReadRandomArray(arr, arrLength);
-  CopyArray(arr, arr2, arrLength,newLength);
+  ReadRandomArray(arr2, arrLength);
   cout
-      << "\nThe copy array: ";
+      << "\nThe first array: ";
   PrintArray(arr, arrLength);
-    cout
-      << "\nThe copy array: ";
-  PrintArray(arr2, newLength);
+  cout
+      << "\nThe second array: ";
+  PrintArray(arr2, arrLength);
+  cout
+      << "\nThe sum of two arrays: ";
+  PrintSumTwoArrays(arr,arr2, arrLength);
+
   return 0;
 }
